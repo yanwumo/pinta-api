e331cbe8f10ade36e1ade18fdf1b67b3c08a1794
yanwumo <yanwumo@outlook.com>
2020-08-19 09:36:14 -0700

    Use kubernetes_asyncio to rewrite WebSocket-related connections.

--- pinta/api/kubernetes/websocket.py
+++ pinta/api/kubernetes/websocket.py
@@ -1,46 +1,70 @@
 import asyncio
-from typing import List, Optional
+from typing import List
 
 import websockets
-from kubernetes import config, client
+from aiohttp import ClientWebSocketResponse, WSMsgType
+from kubernetes_asyncio import client, config
+from kubernetes_asyncio.client.api_client import ApiClient
+from kubernetes_asyncio.stream import WsApiClient
 from starlette.websockets import WebSocket, WebSocketDisconnect
 
-from pinta.api.kubernetes.exec_client import stream
 from pinta.api.core.config import settings
 
 
-async def tunnel1(ws: WebSocket, resp: websockets.WebSocketClientProtocol):
+async def tunnel1(ws: WebSocket, resp: ClientWebSocketResponse):
     while True:
         data = await ws.receive_bytes()
-        await resp.send(data)
+        await resp.send_bytes(data)
 
 
-async def tunnel2(resp: websockets.WebSocketClientProtocol, ws: WebSocket):
+async def tunnel2(resp: ClientWebSocketResponse, ws: WebSocket):
     while True:
-        data = await resp.recv()
+        data = await resp.receive_bytes()
         await ws.send_bytes(data)
 
 
-async def proxy(ws: WebSocket, pod: str, command: List[str], tty: bool, container: str = ""):
+async def exec_proxy(ws: WebSocket, pod: str, command: List[str], tty: bool, container: str = ""):
     if settings.K8S_DEBUG:
-        config.load_kube_config()
+        await config.load_kube_config()
     else:
-        config.load_incluster_config()
-    api = client.CoreV1Api()
-    call_args = stream(api.connect_get_namespaced_pod_exec,
-                       name=pod,
-                       namespace="default",
-                       command=command,
-                       container=container,
-                       stderr=True, stdin=True,
-                       stdout=True, tty=tty,
-                       _preload_content=False)
-    async with websockets.connect(**call_args) as resp:
-        t1 = asyncio.create_task(tunnel1(ws, resp))
-        t2 = asyncio.create_task(tunnel2(resp, ws))
-        try:
-            await asyncio.gather(t1, t2)
-        except websockets.exceptions.ConnectionClosedOK:
-            pass
-        except WebSocketDisconnect:
-            pass
+        await config.load_incluster_config()
+    api = client.CoreV1Api(WsApiClient())
+    resp = await api.connect_get_namespaced_pod_exec(pod,
+                                                     "default",
+                                                     command=command,
+                                                     container=container,
+                                                     stderr=True, stdin=True,
+                                                     stdout=True, tty=tty,
+                                                     _preload_content=False)
+    t1 = asyncio.create_task(tunnel1(ws, resp))
+    t2 = asyncio.create_task(tunnel2(resp, ws))
+    try:
+        await asyncio.gather(t1, t2)
+    except TypeError:
+        pass
+    except WebSocketDisconnect:
+        pass
+    finally:
+        await resp.close()
+
+
+async def log_proxy(ws: WebSocket, pod: str, container: str = ""):
+    if settings.K8S_DEBUG:
+        await config.load_kube_config()
+    else:
+        await config.load_incluster_config()
+    api = client.CoreV1Api(ApiClient())
+    resp = await api.read_namespaced_pod_log(pod,
+                                             "default",
+                                             container=container,
+                                             tail_lines=100,
+                                             follow=True,
+                                             _preload_content=False)
+    try:
+        while True:
+            line = await resp.content.readline()
+            if not line:
+                break
+            await ws.send_bytes(bytes([1]) + line)
+    except websockets.exceptions.ConnectionClosedOK:
+        pass
